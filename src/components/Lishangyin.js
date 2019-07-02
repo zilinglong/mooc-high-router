f1c2fd256deab4cc2eea58acb10dfa4ca81c25e2
mengmingxiu <mengmingxiu@100tal.com>
2019-07-02 16:25:48 +0800

    路由完善

--- src/components/Lishangyin.js
+++ src/components/Lishangyin.js
@@ -4,7 +4,11 @@ class Lishangyin extends Component {
   render() {
     return (
       <div className="content">
-        Lishangyin
+        <h2>锦瑟<i>(李商隐)</i></h2>
+        <p>锦瑟无端五十弦，一弦一柱思华年</p>
+        <p>庄生晓梦迷蝴蝶，望帝春心托杜鹃</p>
+        <p>沧海月明珠有泪，蓝田日暖玉生烟</p>
+        <p>此情可待成追忆，只是当时已惘然</p>
       </div>
     );
   }
